dba7ac5128ac00b6301617b9d10ea2a87f3ac612
Toro Boxen <toroboxen@TORO-iM02.local>
2015-08-12 12:35:38 +0800

    boxen

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,15 @@
-# Public: Install SoapUI.app into /Applications.
-#
-# Examples
-#
-#   include soapui
 class soapui {
 
-  package { 'SoapUI':
-    ensure   => 'installed',
-    provider => 'appdmg',
-    source   => 'http://nchc.dl.sourceforge.net/project/soapui/soapui/5.2.0/SoapUI-5.2.0.dmg'
-  }
+##  package { 'SoapUI':
+##    ensure   => 'installed',
+##    provider => 'appdmg',
+##    source   => 'http://nchc.dl.sourceforge.net/project/soapui/soapui/5.2.0/SoapUI-5.2.0.dmg'
+##  }
+$soap = 'SoapUI-5.2.0-mac-bin.zip'
+$source = 'http://192.168.21.151/SoapUI-5.2.0-mac-bin.zip'
+
+exec { 'install soapui':
+  ensure => absent,
+  command => "cd /Applications/ && curl -O ${source} && open ${soap}",
+}
 }
--- spec/classes/soapui_spec.rb
+++ spec/classes/soapui_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe 'soapui' do
 
-  it do
-    should contain_package('SoapUI').with({
-      :provider => 'appdmg',
-      :source   => 'http://nchc.dl.sourceforge.net/project/soapui/soapui/5.2.0/SoapUI-5.2.0.dmg'
-    })
-  end
+#  it do
+#    should contain_package('SoapUI').with({
+#      :provider => 'appdmg',
+#      :source   => 'http://nchc.dl.sourceforge.net/project/soapui/soapui/5.2.0/SoapUI-5.2.0.dmg'
+#    })
+#  end
 end
